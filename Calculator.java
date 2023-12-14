08eb25b8f2d26c85da6dd3136f84148678dde68f
jhoho82 <jhoho82@gmail.com>
2023-12-14 14:24:23 +0900

    ^^&&&&

--- Calculator.java
+++ Calculator.java
@@ -2,5 +2,12 @@
 public class Calculator {
     public static void main(String[] args) {
 
+
+  	int my_add(int number1, int number2){
+			if (number1 < 0 or number2 < 0)
+				return;
+			return number1+number2;
+		}
+
 }
 
